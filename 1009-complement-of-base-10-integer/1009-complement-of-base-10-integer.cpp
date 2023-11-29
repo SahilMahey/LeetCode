249c06a00a5c36543b4ee25118180706351121b0
SahilMahey <90522898+SahilMahey@users.noreply.github.com>
2023-11-29 15:55:24 -0330

    Time: 0 ms (100.00%), Space: 6.2 MB (76.36%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
     int bitwiseComplement(int n) {
-        int answer = 0;
-        int i = 0;
-        if (n==0)
-            return 1;
-        while (n!=0)
+        int m = n;
+        int mask = 0;
+        if (m==0)
         {
-            answer = answer + (~(n)&1)*pow(2,i);
-            i = i + 1;
-            n= n>>1;
+            return 1;
         }
-         return (answer);
+        
+        while (m!=0)
+        {
+            mask = (mask <<1) | 1;
+            m = m >> 1;
+        }
+         int ans = ((~n) & mask);
+        return ans;
     }
 };
 
-// This was my previous solution. It did not worked out as 127 cases passed but 128 case was what if the number is 0. I can make a if condition but it will just increase the time complexity or memory consumption.
+// This was my previous solution. this did not worked as it was faster than only 8.96% solution.
 
 // int answer = 0;
 //         int i = 0;
+           // if (n==0)
+            //return 1;
 //         while (n!=0)
 //         {
 //             answer = answer + (~(n)&1)*pow(2,i);
