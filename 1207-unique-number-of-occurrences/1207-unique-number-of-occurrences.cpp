de191b198a64c204cedfeef87fd32e083e103890
SahilMahey <90522898+SahilMahey@users.noreply.github.com>
2023-11-30 20:38:35 -0330

    Time: 4 ms (44.25%), Space: 8.4 MB (84.43%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -0,0 +1,26 @@
+//I know this is a not an optimal solution, however, I have not leant hashmap and sets yet. I would come back to this in order to solve these kinds of questions
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        
+        int i = 0;
+        sort(arr.begin(),arr.end());
+        vector<int> ans;
+        while (i < arr.size()){
+            int count = 1;
+            for (int j = i+1; j< arr.size(); j++){
+                if (arr[i] == arr[j])
+                    count++;
+            }
+            ans.push_back(count);
+            i = i + count;
+        }
+        sort(ans.begin(),ans.end());
+        for (int i = 0; i < ans.size() - 1; i++){
+            if (ans[i] == ans [i+1])
+                return false;
+        }
+        return true;
+    
+    }
+};
