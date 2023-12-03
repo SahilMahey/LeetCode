91e70da59b49711f68db08afac05f328f0226190
SahilMahey <90522898+SahilMahey@users.noreply.github.com>
2023-12-03 00:54:39 -0330

    Time: 3 ms (83.94%), Space: 9 MB (7.28%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,22 +1,60 @@
+// This is ths solution I learned
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector <int> ans;
-        
-        for (int i = 0; i <= n ;i++)
+        vector <int> ans = {0};
+        for (int i = 1; i <= n; i++)
         {
-            int count = 0;
-            int j = i;
-            while (j!=0)
-            {
-                if (j&1)
-                {
-                    count++;
-                }
-                j = j >> 1;
-            }
-            ans.push_back(count);
+           int ans_ = ans[i/2] + i%2;
+            ans.push_back(ans_);
         }
-        return ans;
+        
+       return ans; 
     }
 };
+
+
+//This is the solution i did
+
+// vector <int> ans;
+        
+//         for (int i = 0; i <= n ;i++)
+//         {
+//             int count = 0;
+//             int j = i;
+//             while (j!=0)
+//             {
+//                 if (j&1)
+//                 {
+//                     count++;
+//                 }
+//                 j = j >> 1;
+//             }
+//             ans.push_back(count);
+//         }
+//         return ans;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
