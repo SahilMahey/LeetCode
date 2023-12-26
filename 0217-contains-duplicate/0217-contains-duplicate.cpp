53348545536d69b925386faad703873cc14289da
SahilMahey <90522898+SahilMahey@users.noreply.github.com>
2023-12-26 18:52:11 -0330

    Time: 106 ms (42.05%), Space: 72.2 MB (13.22%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         
-        set<int> num(nums.begin(), nums.end());
+        unordered_set<int> num(nums.begin(), nums.end());
         
         return num.size() < nums.size();
     }
