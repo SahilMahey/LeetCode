2b37e64a7f7e43c9a6e9d5f568b6041dd7316672
SahilMahey <90522898+SahilMahey@users.noreply.github.com>
2023-12-31 13:49:29 -0330

    Time: 37 ms (5.35%), Space: 14.2 MB (45.77%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -10,24 +10,17 @@ public:
         }
         for (auto a = count.begin(); a!=count.end(); a++)
         {
-            cout<< a->first << " "<< a->second<<endl;
-            count2[a->second].push_back(a->first);
-            
-            
+           count2[a->second].push_back(a->first);
         }
         
         for (auto b = count2.rbegin(); b!=count2.rend()&&k>0; b++)
         {
-            
-            cout<< b->first<< " ";
-            
             for (auto a:b->second)
             {
                 c.push_back(a);
-                cout << a;
                 k--;
             }
-            cout<<endl;
+           
          }
         
         return c;
