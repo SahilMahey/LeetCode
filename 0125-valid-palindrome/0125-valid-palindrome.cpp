33bc305ee7efa04edd34ed3a53b71b5f975c552b
SahilMahey <90522898+SahilMahey@users.noreply.github.com>
2024-01-01 18:10:29 -0330

    Time: 127 ms (5.05%), Space: 7.6 MB (82.97%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+       int i = 0;
+       int j = s.length()-1;
+       while(i<=j)
+       {
+           cout<<s[i];
+           cout<<s[j];
+           cout<<endl;
+           if (isalnum(s[i]) && isalnum(s[j]) && tolower(s[i])!=tolower(s[j])) 
+           {
+               return false;
+           }
+           else if (!isalnum(s[i]))
+           {
+               i++;
+           }
+           else if (!isalnum(s[j]))
+           {
+              j--;
+           }
+           else
+           {
+               i++;
+               j--;
+           }
+       }
+        return true;
+    }
+};
