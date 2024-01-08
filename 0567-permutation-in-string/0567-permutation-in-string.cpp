e2a77cb4e49896cfcdc9e08a462922f13d2b45f8
SahilMahey <90522898+SahilMahey@users.noreply.github.com>
2024-01-07 20:43:42 -0330

    Time: 2240 ms (5.01%), Space: 58.4 MB (8.31%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if (s2.length()<s1.length())
+        {
+            return false;
+        }
+        int i = 0;
+        string p;
+        while (i <= (s2.length()-s1.length()))
+        {
+            p = s2.substr(i, s1.length());
+            sort(s1.begin(),s1.end());
+            sort(p.begin(),p.end());
+            if(s1==p)
+            {
+                return true;
+            }
+            i++;
+        }
+        return false;
+    }
+};
