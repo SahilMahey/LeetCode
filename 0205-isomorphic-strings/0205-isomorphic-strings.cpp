b59a287bb7fc44541460f2bd706f0a651b2c096a
SahilMahey <90522898+SahilMahey@users.noreply.github.com>
2024-01-10 19:50:19 -0330

    Time: 7 ms (53.51%), Space: 7.4 MB (53.28%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -5,20 +5,14 @@ public:
        unordered_map<char,char>d;
        for (int i = 0; i < s.length(); i++)
        {
+           if ((c[s[i]] && c[s[i]]!=t[i]) ||  (d[t[i]] && d[t[i]]!=s[i]))
+           {
+               return false;
+           }
             c[s[i]] = t[i];
             d[t[i]] = s[i];
            
        }
-       
-        for (int i = 0; i < s.length(); i++)
-       {
-            if (c[s[i]]!=t[i] || d[t[i]]!=s[i])
-            {
-                return false;
-            }
-       }
-        
-        
         return true;
     }
 };
