1f90c22e0bf457a5de11913c25ff3f43d0f0d71e
SahilMahey <90522898+SahilMahey@users.noreply.github.com>
2024-01-12 04:03:50 -0330

    Time: 326 ms (11.61%), Space: 58.5 MB (7.24%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -8,11 +8,11 @@ public:
         
        vector<int>a(26,0);
        vector<int>b(26,0);
-       int ch_num;
+      
        for (int i = 0; i < s1.length(); i++)
        {
-           ch_num = s1[i] - 'a';
-           a[ch_num]++;
+           
+           a[s1[i] - 'a']++;
        }
        string m = " ";
        for (int j = 0; j <= s2.length()-s1.length(); j++)
@@ -20,8 +20,8 @@ public:
            m = s2.substr(j,s1.length());
            for (int k = 0; k < m.length(); k++)
            {
-                ch_num = m[k] - 'a';
-                b[ch_num]++;
+                
+                b[m[k] - 'a']++;
            }
            if (a == b)
            {
@@ -29,8 +29,8 @@ public:
            }
            for (int k = 0; k < m.length(); k++)
            {
-                ch_num = m[k] - 'a';
-                b[ch_num] = 0;
+                
+                b[m[k] - 'a'] = 0;
            }
            
        }
