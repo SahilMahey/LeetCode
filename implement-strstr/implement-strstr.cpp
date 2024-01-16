fd1bc30811cdcdf3c7465fd5e5120e02ef831b5b
SahilMahey <90522898+SahilMahey@users.noreply.github.com>
2024-01-16 19:16:34 -0330

    Time: 3 ms, Memory: 6.7 MB - LeetHub

---
+++ implement-strstr/implement-strstr.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if (needle.length()>haystack.length())
+        {
+            return -1;
+        }
+        if (haystack.find(needle)!=string::npos)
+        {
+            return haystack.find(needle);
+        }
+        return -1 ;
+    }
+};
