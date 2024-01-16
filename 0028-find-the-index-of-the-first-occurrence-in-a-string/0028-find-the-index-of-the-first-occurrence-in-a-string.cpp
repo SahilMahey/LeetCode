50e471682518e2e4cd40101270b66aa3e84cb3a2
SahilMahey <90522898+SahilMahey@users.noreply.github.com>
2024-01-16 19:18:04 -0330

    Time: 3 ms (45.53%), Space: 6.6 MB (87.67%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if (needle.length()>haystack.length())
+        {
+            return -1;
+        }
+        int index =  haystack.find(needle);
+        if (haystack.find(needle)!=string::npos)
+        {
+            return haystack.find(needle);
+        }
+        return -1 ;
+    }
+};
